589fe084ac7b08cbf9c78e49a42484723cd2394b
rakesh-paulraj1 <rakeshtherock2004@gmail.com>
2024-06-30 23:12:34 +0530

    done

--- app.py
+++ app.py
@@ -1,40 +1,14 @@
-import requests
-import json
-import gradio as gr
-
-url="http://localhost:11434/api/generate"
-
-headers={
-
-    'Content-Type':'application/json'
-}
-
-history=[]
-
-def generate_response(prompt):
-    history.append(prompt)
-    final_prompt="\n".join(history)
-
-    data={
-        "model":"codeg",
-        "prompt":final_prompt,
-        "stream":False
-    }
-
-    response=requests.post(url,headers=headers,data=json.dumps(data))
-
-    if response.status_code==200:
-        response=response.text
-        data=json.loads(response)
-        actual_response=data['response']
-        return actual_response
-    else:
-        print("error:",response.text)
-
-
-interface=gr.Interface(
-    fn=generate_response,
-    inputs=gr.Textbox(lines=4,placeholder="Enter your Prompt"),
-    outputs="text"
+from langchain_community.llms import LlamaCpp
+from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+from langchain_core.prompts import PromptTemplate
+# Make sure the model path is correct for your system!
+llm = LlamaCpp(
+    model_path="/Users/rlm/Desktop/Code/llama.cpp/models/openorca-platypus2-13b.gguf.q4_0.bin",
+    n_gpu_layers=n_gpu_layers,
+    n_batch=n_batch,
+    callback_manager=callback_manager,
+    verbose=True,  # Verbose is required to pass to the callback manager
 )
-interface.launch()
+llm_chain = prompt | llm
+question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
+llm_chain.invoke({"question": question})
